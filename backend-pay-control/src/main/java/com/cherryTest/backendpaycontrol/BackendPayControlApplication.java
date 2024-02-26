c5ce8eba2a3d0f05484a896ee900647c356d0d27
FelipeCherry3 <felipeelias609@hotmail.com>
2024-02-26 15:36:34 -0300

    Transaction Model

---
+++ backend-pay-control/src/main/java/com/cherryTest/backendpaycontrol/BackendPayControlApplication.java
@@ -0,0 +1,13 @@
+package com.cherryTest.backendpaycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPayControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPayControlApplication.class, args);
+	}
+
+}
---
+++ backend-pay-control/src/main/java/com/cherryTest/backendpaycontrol/domain/transaction/Transaction.java
@@ -0,0 +1,29 @@
+package com.cherryTest.backendpaycontrol.domain.transaction;
+
+import com.cherryTest.backendpaycontrol.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    private User sender;
+    @ManyToOne
+    private User receiver;
+
+}
